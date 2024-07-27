15c823280597876db124a760282f0e5b5541ae92
sleepjie <lugaruuuu@gmail.com>
2024-07-27 01:07:30 +0100

    itemnames
    removed brackets for weapon names causing errors

--- gamemode/schema/items/weapon/sh_laserrifle(wbeamspliter) - Copy.lua
+++
@@ -1,6 +0,0 @@
-ITEM.name = "Laser Rifle (w/Beam Splitter and Scope)"
-ITEM.class = "weapon_laserriflebsscp"
-ITEM.model = Model("models/halokiller38/fallout/weapons/energy weapons/laserriflescopebs.mdl")
-ITEM.desc = "A Semi-Automatic laser rifle that is powered by microfusion cells and has been modified with a beam splitter and scope."
-ITEM.weight = 7
-ITEM.price = 2900
